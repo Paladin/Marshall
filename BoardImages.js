000350ae7acaef6b5825e562a22b89c6476e4e15
Paladin <support@paladinweb.com>
2011-07-05 15:22:43 -0500

    Oops
    Fixed a bad BoardImages edit, added the development css, and cleaned up the dev.html code

--- BoardImages.js
+++ BoardImages.js
@@ -15,7 +15,59 @@
  * limitations under the License.
 **/
 
-function isYahoo(pgn) {
-    pgn = pgn.replace(/^\s+|\s+$/g, '');
-    return pgn.charAt(0) == ';'; 
-}
+	function BoardImages(options) {
+		this.set = "default";
+		this.pref = "";
+		this.suf = 'gif';
+		if (options['set']) {
+			this.set = options['set'];
+		}
+		if (options['imagePrefix']) {
+			this.pref = options['imagePrefix'];
+		}
+		if (options['imageSuffix']) {
+			this.suf = options['imageSuffix'];
+		}
+		this.imageNames = {
+			"default": {
+				"white" : {"rook":"wRook."+this.suf
+									 ,"bishop":"wBishop."+this.suf
+									 ,"knight":"wKnight."+this.suf
+									 ,"queen":"wQueen."+this.suf
+									 ,"king":"wKing."+this.suf
+									 ,"pawn":"wPawn."+this.suf}
+					
+				,"black" : {"rook":"bRook."+this.suf
+									 ,"bishop":"bBishop."+this.suf
+									 ,"knight":"bKnight."+this.suf
+									 ,"queen":"bQueen."+this.suf
+									 ,"king":"bKing."+this.suf
+									 ,"pawn":"bPawn."+this.suf}
+
+				,"btns" : {"ffward":"buttons/ffward."+this.suf
+										,"rwind":"buttons/rwind."+this.suf
+										,"forward":"buttons/forward."+this.suf
+										,"back":"buttons/back."+this.suf
+										,"toggle":"buttons/toggle."+this.suf
+										,"comments":"buttons/comments."+this.suf
+										,"flip":"buttons/flip."+this.suf
+										}
+			}
+		};
+
+		this.preload = function() {
+			var set = this.set;
+			var pref = this.pref;
+			if (arguments.length>0)
+				set = arguments[0];
+			if (arguments.length>1)
+				pref = arguments[1];
+			var img;
+			for (var i in this.imageNames[set]) {
+				for (var j in this.imageNames[set][i]) {
+					img = new Image();
+					img.src = this.imageNames[set][i][j];
+				}
+			}
+		};
+	};
