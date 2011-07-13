be4fe2a28dd523f9a7b297ca99d8fc0a509ed653
Paladin <support@paladinweb.com>
2011-07-13 11:57:57 -0500

    Started testing Converter
    Long range this should probably be part of PGN object, but for now let's get some tests in place.

---
+++ test/converter_test.js
@@ -0,0 +1,31 @@
+TestCase( "ConverterTest", 
+{
+	goodPGN: 	'[Event	"Dayton"]' +
+				'[Site	"?"]\n' +
+				'[Date	"1890.02.25"]\n' +
+				'[Round	"?"]\n' +
+				'[White	"Blumenschein, E.."]\n' +
+				'[Black	"Smith, W.H.."]\n' +
+				'[Result	"*"]\n' +
+				'[Annotator	"Reeh,Oliver"]\n' +
+				'[SetUp	"1"]\n' +
+				'[FEN	"r3qr1k/ppp1nbpB/1b1p3B/4p2Q/3P1n1N/2P4R/PP1N2PP/R5K1 w - - 0 13"]\n' +
+				'[PlyCount	"9"]\n' +
+				'[SourceDate	"2009.06.14"]\n' +
+				'{In this position White has a beautiful focred mate in five, so answer C is correct:} 1. Bxg7+ $1 Kxg7 2. Qh6+ $3 {The point - Black\'s king is dragged into a	discovered double-check.} Kxh6 (2... Kh8 3. Bg6+ Kg8 4. Qh7#) 3. Nf5+ Kg5 4. Ne4+ Kg4 5. Rh4# {or 5.Ne3++. This brilliancy was published far more than 100 years ago - in May 1890 on page 155 of International Chess Magazine. (Source: "A Chess Omnibus", Edward Winter, Russel Enterprises, Inc., 2003)} *',
+	
+
+	setup: function() {},
+	teardown: function() {},
+	
+	"test default converter creation": function() {
+		pgn = new Pgn( this.goodPGN );
+		conv = new Converter(pgn);
+		
+		assertEquals( pgn, conv.pgn );
+		assertEquals( "vSquare -- piece = knight color=white type=", conv.vBoard[4][7].toString() );
+		assertEquals( "vSquare -- piece = knight color=white type=", conv.initialBoard[4][7].toString() );
+		assertTrue( conv.whiteToMove );
+		assertEquals( 13, conv.startMoveNum );
+	}
+});
