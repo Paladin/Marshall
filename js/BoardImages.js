ecae0671fbb9861931880127f3b2a76842710e8d
Paladin <support@paladinweb.com>
2012-07-30 19:35:18 -0500

    Code style revamp
    eliminated unused variable

--- js/BoardImages.js
+++ js/BoardImages.js
@@ -1,79 +1,80 @@
-/** Version: 0.7.1 **/
 /**
- * Copyright 2008 Toomas Ršmer
+ * The images used on the chess board
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * @constructor
+ * @param {array} options The configuration options
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * @property {string}	set		    - Name of the set of piece images
+ * @property {string}	pref
+ * @property {string}	suf		    - The image suffix to use
+ * @property {array}	imageNames	- 2D array of image file names
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-**/
+ * @version 0.7.1
+ * @author Toomas Ršmer
+ * @author Arlen P Walker
+ * @copyright 2008 Toomas Ršmer
+ * @copyright 2012 Arlen P Walker (some portions)
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ **/
 function BoardImages(options) {
-	this.set = "default";
-	this.pref = "";
-	this.suf = 'gif';
-	if (options['set']) {
-		this.set = options['set'];
-	}
-	if (options['imagePrefix']) {
-		this.pref = options['imagePrefix'];
-	}
-	if (options['imageSuffix']) {
-		this.suf = options['imageSuffix'];
-	}
+    "use strict";
+	this.set = options.set || "default";
+	this.pref = options.imagePrefix || "";
+	this.suf = options.imageSuffix || "gif";
+
 	this.imageNames = {
 		"default": {
-			"white" : {"rook":"wRook."+this.suf
-								 ,"bishop":"wBishop."+this.suf
-								 ,"knight":"wKnight."+this.suf
-								 ,"queen":"wQueen."+this.suf
-								 ,"king":"wKing."+this.suf
-								 ,"pawn":"wPawn."+this.suf
-						}
-				
-			,"black" : {"rook":"bRook."+this.suf
-								 ,"bishop":"bBishop."+this.suf
-								 ,"knight":"bKnight."+this.suf
-								 ,"queen":"bQueen."+this.suf
-								 ,"king":"bKing."+this.suf
-								 ,"pawn":"bPawn."+this.suf
-						}
+			"white" : {
+			    "rook": "wRook." + this.suf,
+				"bishop": "wBishop." + this.suf,
+				"knight": "wKnight." + this.suf,
+				"queen": "wQueen." + this.suf,
+				"king": "wKing." + this.suf,
+				"pawn": "wPawn." + this.suf
+			},
 
-			,"btns" : {"ffward":"buttons/ffward."+this.suf
-									,"rwind":"buttons/rwind."+this.suf
-									,"forward":"buttons/forward."+this.suf
-									,"back":"buttons/back."+this.suf
-									,"toggle":"buttons/toggle."+this.suf
-									,"comments":"buttons/comments."+this.suf
-									,"flip":"buttons/flip."+this.suf
-									,"up":"buttons/up."+this.suf
-									,"down":"buttons/down."+this.suf
-						}
-					}
-	}
-}
+			"black" : {
+			    "rook": "bRook." + this.suf,
+				"bishop": "bBishop." + this.suf,
+				"knight": "bKnight." + this.suf,
+				"queen": "bQueen." + this.suf,
+				"king": "bKing." + this.suf,
+				"pawn": "bPawn." + this.suf
+			},
 
-BoardImages.prototype.preload = function() {
-		var set = this.set;
-		var pref = this.pref;
-		if (arguments.length>0)
-			set = arguments[0];
-		if (arguments.length>1)
-			pref = arguments[1];
-		var img;
-		var img_count = 0;
-		for (var i in this.imageNames[set]) {
-			for (var j in this.imageNames[set][i]) {
-				img = new Image();
-				img.src = this.imageNames[set][i][j];
-				img_count++;
+			"btns" : {
+			    "ffward": "buttons/ffward." + this.suf,
+				"rwind":    "buttons/rwind." + this.suf,
+				"forward":  "buttons/forward." + this.suf,
+				"back": "buttons/back." + this.suf,
+				"toggle":   "buttons/toggle." + this.suf,
+				"comments": "buttons/comments." + this.suf,
+				"flip": "buttons/flip." + this.suf,
+				"up":   "buttons/up." + this.suf,
+				"down": "buttons/down." + this.suf
 			}
 		}
-		return img_count;
-	}
+	};
+}
+
+BoardImages.prototype.preload = function (theSet) {
+    "use strict";
+    var set = arguments.length > 0 ? theSet : this.set,
+        i,
+        j,
+        img,
+        img_count = 0;
+
+    for (i in this.imageNames[set]) {
+        if (this.imageNames[set].hasOwnProperty(i)) {
+            for (j in this.imageNames[set][i]) {
+                if (this.imageNames[set][i].hasOwnProperty(j)) {
+                    img = new Image();
+                    img.src = this.imageNames[set][i][j];
+                    img_count += 1;
+                }
+            }
+        }
+    }
+    return img_count;
+};
