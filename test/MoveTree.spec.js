a1accb1e0601563fa53dfe61a9009f984c988ef4
Paladin <support@paladinweb.com>
2012-08-08 08:30:50 -0500

    Improved testing of this object

--- test/MoveTree.spec.js
+++ test/MoveTree.spec.js
@@ -34,7 +34,10 @@
         d4b = { number: 4, text: "Bg7", color: "black" },
         d5w = { number: 5, text: "Bc4", color: "white" },
         e4b = { number: 4, text: "d6", color: "black" },
-        e5w = { number: 5, text: "Bb5", color: "white" };
+        e5w = { number: 5, text: "Bb5", color: "white" },
+        f2w = { number: 5, text: "Nf3", color: "white" },
+        f2b = { number: 5, text: "Nc6", color: "black" },
+        f3w = { number: 5, text: "d4", color: "white" };
 
     	beforeEach(function () {
     	    this.moveTree = new MoveTree(a1w);
@@ -47,6 +50,9 @@
     	    this.moveTree.next.next.next.next.next.next.addNext(a4b);
     	    this.moveTree.next.next.addVariation(b2w);
     	    this.moveTree.next.next.down.addNext(b2b);
+    	    this.moveTree.next.next.down.addVariation(f2w);
+    	    this.moveTree.next.next.down.down.addNext(f2b);
+    	    this.moveTree.next.next.down.down.next.addNext(f3w);
     	    this.moveTree.next.next.down.next.addNext(b3w);
     	    this.moveTree.next.next.down.next.next.addVariation(c3w);
     	    this.moveTree.next.next.next.next.next.addVariation(d3b);
@@ -72,6 +78,29 @@
     	});
     	it(" Should get next move", function () {
     		expect(this.moveTree.getNextMove()).toBe(this.moveTree.next);
+    	});
+    	it(" Should not be at the end of a line", function () {
+    		expect(this.moveTree.isEnd()).toBe(false);
+    	});
+    	it(" Should be at the end of a line", function () {
+    		expect(this.moveTree.next.next.down.next.next.isEnd()).toBe(true);
+    	});
+    	it(" Should got to the start of the line", function () {
+    		expect(this.moveTree.next.next.next.next.next.down.next.next.next
+    		    .goStart()).toBe(this.moveTree.next.next.next.next.next.down);
+    	});
+    	it(" Should go to the end of the line", function () {
+    		expect(this.moveTree.goEnd()).
+    		    toBe(this.moveTree.next.next.next.next.next.next.next);
+    	});
+    	it(" Should got to last subvariation of this move", function () {
+    		expect(this.moveTree.next.next.goBottom())
+    		    .toBe(this.moveTree.next.next.down.down);
+    	});
+    	it(" Should come up to the parent variation", function () {
+    		expect(this.moveTree.next.next.next.next.next.down.next.next.
+    		    down.goTop()).toBe(this.moveTree.next.next.next.next.next.
+    		    down.next.next);
     	});
     });
 } ());
