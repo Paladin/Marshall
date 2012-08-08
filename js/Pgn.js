c354d1904c4ce3394acb99d63b256c457123b1bc
Paladin <support@paladinweb.com>
2012-08-08 12:00:52 -0500

    Restricted regex to expect proper chars in tag labels
    Regex allows normal word chars, spaces both slashes(\/), periods, colons dashes question marks and asterisks.

--- js/Pgn.js
+++ js/Pgn.js
@@ -419,7 +419,7 @@ Pgn.prototype = {
 
         tag = text.substring(0, text.indexOf("]"));
         text = text.slice(tag.length);
-        tag = tag.match(/([\w]*)\s\"((.)*)\"/);
+        tag = tag.match(/([\w]*)\s\"([\w \\\/,\.:\-\?\*]*)\"/);
         this.props[tag[1]] = tag[2];
         return text;
     },
